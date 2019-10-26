b61189a25544baf43b3691eb9829ddb092ff3a36
James Grantham <james.r.grantham@gmail.com>
2019-10-26 09:21:59 +0100

    Create axios for login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { Formik, Field, Form } from 'formik';
+import axios from 'axios';
+
+const loginApi = 'http://localhost:5000/api/login';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const LoginForm = props => {
+
+    const onLogin = ({username, password}) => {
+      axios
+        .post(loginApi, {username, password})
+        .then(response => {
+          console.log(response)
+          localStorage.setItem('token', response.data.payload);
+          props.history.push('/bubbles')
+        .catch(error => {
+          alert(error.response.data.message)
+        })
+        })
+    }
+  }
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
