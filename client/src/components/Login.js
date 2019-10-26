ebcd5bf12a41b5eabad9784e2044b1dd04fb119f
James Grantham <james.r.grantham@gmail.com>
2019-10-26 09:48:31 +0100

    Create form for login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,34 +1,40 @@
 import React from "react";
-import { Formik, Field, Form } from 'formik';
-import axios from 'axios';
+import { Formik, Field, Form } from "formik";
+import axios from "axios";
 
-const loginApi = 'http://localhost:5000/api/login';
+const loginApi = "http://localhost:5000/api/login";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const LoginForm = props => {
-
-    const onLogin = ({username, password}) => {
-      axios
-        .post(loginApi, {username, password})
-        .then(response => {
-          console.log(response)
-          localStorage.setItem('token', response.data.payload);
-          props.history.push('/bubbles')
-        .catch(error => {
-          alert(error.response.data.message)
-        })
-        })
-    }
-  }
-
+  const onLogin = ({ username, password }) => {
+    axios
+      .post(loginApi, { username, password })
+      .then(response => {
+        console.log(response);
+        localStorage.setItem("token", response.data.payload);
+        props.history.push("/bubbles")
+      // .catch(error => {
+      //   console.log(error);
+      // });
+    });
+  };
 
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={onLogin}
+        render={() => (
+          <Form>
+            <Field name="username" type="text" placeholder="Username" />
+            <Field name="password" type="text" placeholder="Password" />
+            <button type='Submit'>Submit</button>
+          </Form>
+        )}
+      />
     </>
   );
 };
